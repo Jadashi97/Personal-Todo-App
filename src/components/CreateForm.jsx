714459d7c732e2ffbed0b8dd775c6f31c2ab275b
jadashi97 <njadauniversal23@yahoo.com>
2023-04-10 12:07:40 -0500

    (feat): added css and some logic to handle the todo input. (fix): complete Create part

--- src/components/CreateForm.jsx
+++ src/components/CreateForm.jsx
@@ -1,24 +1,77 @@
 import React, { useState } from 'react'
+import Todo from "../components/Todo";
 
 export default function CreateForm() {
+  const [items, setItems] = useState([]);
   const [todo, setTodo] = useState(" ");
 
   const handleChange = (e) => {
     let value = e.target.value;
-    console.log("here is where we can see the value")
+    console.log(value);
     setTodo(value);
+  }
+
+
+  const handleSubmit = (e) => {  
+    e.preventDefault();
+
+    setItems([...items, todo]); 
+    console.log(setItems());
+    setTodo(" ");
+
   }
 
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input 
           type="text" 
           value={todo} 
           onChange={handleChange} 
           placeholder='add next todo' />
-        <button>Add</button>
+        <button type='submit'>Add</button>
       </form>
+      <ul>
+        {items.map((item, index)=> (
+          <li key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
+
+// import React, { useState } from 'react';
+
+// function Todo() {    
+//   const [items, setItems] = useState([]);
+//   const [text, setText] = useState('');
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setItems([...items, text]);
+//     setText('');
+//   };
+
+//   return (
+//     <div>
+//       <h1>Todo App</h1>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           value={text}
+//           onChange={(e) => setText(e.target.value)}
+//         />
+//         <button type="submit">Add Item</button>
+//       </form>
+//       <ul>
+//         {items.map((item, index) => (
+//           <li key={index}>{item}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Todo;
